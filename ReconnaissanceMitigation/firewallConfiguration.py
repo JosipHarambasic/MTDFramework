1dc57712f54154fbbe2075d759354a6291647ce1
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2023-01-04 16:32:01 +0100

    update

--- ReconnaissanceMitigation/firewallConfiguration.py
+++ ReconnaissanceMitigation/firewallConfiguration.py
@@ -7,8 +7,7 @@ def main():
     subprocess.Popen("iptables -X", shell=True, stdin=subprocess.PIPE)
 
     # adding actual rules
-    firewallConfiguration = [" -A INPUT -i lo -p all -j ACCEPT",
-                             " -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT",
+    firewallConfiguration = [" -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT",
                              " -A INPUT -m recent --name portscan --rcheck --seconds 86400 -j DROP",
                              " -A FORWARD -m recent --name portscan --rcheck --seconds 86400 -j DROP",
                              " -A INPUT -m recent --name portscan --remove",
@@ -19,22 +18,7 @@ def main():
                              " -A FORWARD -p tcp -i eth0 -m state --state NEW -m recent --set",
                              " -A FORWARD -p tcp -i eth0 -m state --state NEW -m recent --update --seconds 60 "
                              "--hitcount 3 -j DROP",
-                             " -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT",
-                             " -A INPUT -p udp -m udp --dport 53 -j ACCEPT",
-                             " -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT",
-                             " -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT",
-                             " -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT",
-                             " -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT",
-                             " -A INPUT -j REJECT",
-                             " -A OUTPUT -o lo -j ACCEPT",
-                             " -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT",
-                             " -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT",
-                             " -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT",
-                             " -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT",
-                             " -A OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT",
-                             " -A OUTPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT",
-                             " -A OUTPUT -j REJECT",
-                             " -A FORWARD -j REJECT"
+
                              ]
     print("Adding rules to iptables")
     counter = 0
