a2f06b90991ace7ba17c997db5072a9fc701619a
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2023-01-04 17:43:34 +0100

    upate

--- ReconnaissanceMitigation/recon.sh
+++ ReconnaissanceMitigation/recon.sh
@@ -27,10 +27,10 @@ iptables -A FORWARD -m state --state INVALID -j DROP
 ### flooding of RST packets, smurf attack Rejection
 #iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
 
-### Protecting against portscans
+### Protecting against port scans
 ### Attacking IP will be locked for 24 hours (3600 x 24 = 86400 Seconds)
-iptables -A INPUT -m recent --name portscan --rcheck --seconds 1500 -j DROP
-iptables -A FORWARD -m recent --name portscan --rcheck --seconds 1500 -j DROP
+iptables -A INPUT -m recent --name portscan --rcheck --seconds 86400 -j DROP
+iptables -A FORWARD -m recent --name portscan --rcheck --seconds 86400 -j DROP
 
 ### Remove attacking IP after 24 hours
 iptables -A INPUT -m recent --name portscan --remove
