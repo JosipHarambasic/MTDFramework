8b341d490b81c308d0cb38479ef0b3ea9d6937cc
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2023-01-04 08:09:56 +0100

    update

--- ReconnaissanceMitigation/recon.py
+++ ReconnaissanceMitigation/recon.py
@@ -7,9 +7,10 @@ def main():
     subprocess.Popen("ifconfig eth0 down", shell=True, stdin=subprocess.PIPE)
 
     # The prefix is needed by nmap to identify the Processor and OS on the device
-    # By changing it to a random other prefix that exists we can obfuscate the attacker
+    # By changing it to a random other prefix that exists we can obfuscate the attacker (Intel, Apple) prefix
     prefix = random.choice(["C8:86:29", "F0:9C:D7", "EC:D6:8A", "EC:23:68", "E0:61:B2", "DC:15:DB", "D4:EC:86",
-                            "CC:8C:DA", "CC:2A:80", "C0:8B:6F", "68:3C:7D"])
+                            "CC:8C:DA", "CC:2A:80", "C0:8B:6F", "68:3C:7D",
+                            "94:0C:98", "90:FD:61", "90:E1:7B", "90:DD:5D", "90:C1:C6", "90:B9:31", "90:B2:1F"])
     newMACAddress = ":%02x:%02x:%02x" % (random.randint(0, 255),
                                          random.randint(0, 255),
                                          random.randint(0, 255))
