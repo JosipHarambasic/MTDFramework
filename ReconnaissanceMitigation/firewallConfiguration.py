0910455c9c8cbab05dae1994f1d77a7b7d66f91d
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2023-01-04 16:19:49 +0100

    update

--- ReconnaissanceMitigation/firewallConfiguration.py
+++ ReconnaissanceMitigation/firewallConfiguration.py
@@ -7,7 +7,8 @@ def main():
     subprocess.Popen("iptables -X", shell=True, stdin=subprocess.PIPE)
 
     # adding actual rules
-    firewallConfiguration = [" -A INPUT -m recent --name portscan --rcheck --seconds 86400 -j DROP",
+    firewallConfiguration = [" -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT",
+                             " -A INPUT -m recent --name portscan --rcheck --seconds 86400 -j DROP",
                              " -A FORWARD -m recent --name portscan --rcheck --seconds 86400 -j DROP",
                              " -A INPUT -m recent --name portscan --remove",
                              " -A FORWARD -m recent --name portscan --remove",
@@ -17,6 +18,7 @@ def main():
                              " -A FORWARD -p tcp -i eth0 -m state --state NEW -m recent --set",
                              " -A FORWARD -p tcp -i eth0 -m state --state NEW -m recent --update --seconds 60 "
                              "--hitcount 3 -j DROP",
+                             " -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT",
                              ]
     print("Adding rules to iptables")
     counter = 0
